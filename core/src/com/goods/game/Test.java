011cc9d43ccd7bc994871e704063fc8b928c9271
Fabi <naass@mail.hs-ulm.de>
2017-04-24 20:09:34 +0200

    shaderTest

--- core/src/com/goods/game/Test.java
+++ core/src/com/goods/game/Test.java
@@ -3,8 +3,6 @@ package com.goods.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.FileHandleResolver;
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Mesh;
@@ -14,22 +12,24 @@ import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
-import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.graphics.g3d.Shader;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.BaseLight;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.model.NodePart;
+import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.DefaultTextureBinder;
 import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.graphics.g3d.utils.RenderContext;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.utils.Pools;
 import com.goods.game.Terrain.Terrain;
@@ -58,6 +58,10 @@ public class Test extends ApplicationAdapter {
     BitmapFont font;
     Texture texture;
     TerrainWithObjects terrainWithObjects;
+    public Shader shader;
+
+    public RenderContext renderContext;
+Renderable renderable;
 
     public boolean loading;
     @Override
@@ -91,7 +95,7 @@ public class Test extends ApplicationAdapter {
         perCam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         // Position rechtsX  hochY  zurückZ
         // Da aber nicht die kamera sonder alles was gerendet wird verschoben wird bedeuten 0,0,10 das alles plus 10 verschpben wird kamera bleibt 0,0,0 ?
-        perCam.position.set(0f, 0f, 300f);
+        perCam.position.set(0f, 0f, 10);
         // 0,0,0 also mitte vom koordinatensys
         perCam.lookAt(0,0,0);
         // ???
@@ -112,11 +116,22 @@ public class Test extends ApplicationAdapter {
         //terrainWithObjects = new TerrainWithObjects(terrainBig);
         ModelBuilder modelBuilder = new ModelBuilder();
         model = modelBuilder.createBox(5f, 5f, 5f, new Material(ColorAttribute.createDiffuse(Color.GREEN)), VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-
+        renderContext = new RenderContext(new DefaultTextureBinder(DefaultTextureBinder.WEIGHTED, 1));
         instances.add(new ModelInstance(model));
         instance = new ModelInstance(model);
-        map = createMesh(400,400,100,false,new Material(ColorAttribute.createDiffuse(Color.RED)));
+        map = createTerrain(1,1,1,false,new Material(ColorAttribute.createDiffuse(Color.RED)));
         instanceArea = new ModelInstance(map);
+        NodePart blockPart = map.nodes.get(0).parts.get(0);
+
+        renderable = new Renderable();
+        blockPart.setRenderable(renderable);
+        renderable.environment = null;
+        renderable.worldTransform.idt();
+        String vert = Gdx.files.internal("shader/vertexShader.glsl").readString();
+        String frag = Gdx.files.internal("shader/fragmentShader.glsl").readString();
+        shader = new DefaultShader(renderable, new DefaultShader.Config(vert, frag));
+        shader.init();
+
         //textTransformation.idt().scl(0.2f).rotate(0, 0, 1, 45).translate(-50, 2, 25f);
         assetManager.load("models/leaftree_1.g3db",Model.class);
         loading = true;
@@ -141,7 +156,7 @@ public class Test extends ApplicationAdapter {
 
 
 
-    private Model createMesh(int width, int height, int numberOfChunks, boolean withHeight, Material material) {
+    private Model createTerrain(int width, int height, int numberOfChunks, boolean withHeight, Material material) {
         int offset = 0;
         float width_half = width / 2;
         float height_half = height / 2;
@@ -158,7 +173,7 @@ public class Test extends ApplicationAdapter {
 
         ModelBuilder builder = new ModelBuilder();
         builder.begin();
-        MeshPartBuilder meshPart = builder.part("top", Gdx.gl30.GL_TRIANGLES, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal, material);
+        MeshPartBuilder meshPart = builder.part("top", Gdx.gl30.GL_TRIANGLES, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates, material);
 
 
         for (int iy = 0; iy < gridY1; iy++) {
@@ -173,10 +188,10 @@ public class Test extends ApplicationAdapter {
 
                 MeshPartBuilder.VertexInfo info = Pools.obtain(MeshPartBuilder.VertexInfo.class);
                 //info.setCol(MathUtils.random(),MathUtils.random(),MathUtils.random(),0);
-               // info.setCol(Color.YELLOW);
+                info.setCol(Color.YELLOW);
                 info.setPos(x,-y,0);
-                info.setNor(0,0,1);
-               // info.setUV(null);
+                info.setNor(0,0,1f);
+                info.setUV(0,1);
                 meshPart.vertex(info);
                 Pools.free(info);
 
@@ -184,8 +199,8 @@ public class Test extends ApplicationAdapter {
                 short b = (short) (ix + gridX1 * (iy + 1));
                 short c = (short) ((ix + 1) + gridX1 * (iy + 1));
                 short d = (short) ((ix + 1) + gridX1 * iy);
-
-                meshPart.index(a,b,d,b,c,d);
+                if(iy !=gridY1-1)
+                    meshPart.index(a,b,d,b,c,d);
                // meshPart.index(a,d,c,b,d,d);
 
             }
@@ -209,18 +224,20 @@ public class Test extends ApplicationAdapter {
         Gdx.gl30.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl30.glClear(GL30.GL_COLOR_BUFFER_BIT|GL30.GL_DEPTH_BUFFER_BIT);
 
-        modelBatch.begin(perCam);
-        modelBatch.render(terrainBig);
-        modelBatch.end();
 
-        modelBatch.begin(perCam);
-        modelBatch.render(instance, environment);
-        modelBatch.end();
-/*
-        modelBatch.begin(perCam);
-        modelBatch.render(instanceArea,environment);
-        modelBatch.end();
-*/
+        renderContext.begin();
+        shader.begin(perCam, renderContext);
+        shader.render(renderable);
+        shader.end();
+        renderContext.end();
+
+     //   modelBatch.begin(perCam);
+        //modelBatch.render(terrainBig);
+        //modelBatch.render(instanceArea);
+        //modelBatch.render(instance);
+       // modelBatch.render(renderable);
+        //modelBatch.end();
+
 
         Gdx.gl30.glEnable(GL30.GL_DEPTH_TEST);
         //spriteBatch.setProjectionMatrix(perCam.combined.mul(textTransformation));
